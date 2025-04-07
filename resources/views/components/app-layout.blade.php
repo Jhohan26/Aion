01cb5839def7d8ab882ad64eddca34486be517fc
Jhohan26 <Jhohan.reyes@uniminuto.edu.co>
2025-04-06 21:13:41 -0500

    Se ha creado la vista de login y el controlador de usuarios

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -1,4 +1,7 @@
-@props(["title" => ""])
+@props([
+	"title" => "",
+	"css" => ""
+])
 
 <!DOCTYPE html>
 <html lang="es">
@@ -6,15 +9,17 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Aion - {{$title}}</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/'.$css.'.css') }}">
 </head>
 <body>
 	{{$slot}}
-	<footer>
+<!-- 	<footer>
 		<div class="bg-gray-800 text-white py-4 mt-8">
 			<div class="container mx-auto text-center">
 				<p>&copy; {{ date('Y') }} Aion. Los derechos son libres hasta el momento que se elimine el repositorio.</p>
 			</div>
 		</div>
-	</footer>
+	</footer> -->
 </body>
 </html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout title="Registrate" css="login">
+	<form>
+		<h1>Iniciar Sesión</h1>
+		<h6>Bienvenido de nuevo, ¡te estábamos esperando!</h6>
+
+		<div class="inputs">
+			<input type="text" name="usuario" placeholder="Usuario / Email">
+			<input type="text" name="contrasena" placeholder="Contraseña">
+			<input class="iniciar" type="submit" value="Iniciar Sesión">
+			<p>o</p>
+			<input class="registrate" type="button" value="Regístrate">
+		</div>
+
+		<a href="">¿Has olvidado tu contraseña?</a>
+	</form>
+</x-app-layout>
