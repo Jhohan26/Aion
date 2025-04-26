748d4cfe5d8734ae64d64390a185f53723316468
Jhohan26 <Jhohan.reyes@uniminuto.edu.co>
2025-04-25 20:04:19 -0500

    Se ha programado los seeders para los productos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Usuario;
 use App\Models\Restaurante;
 use App\Models\Categoria;
+use App\Models\Producto;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 
@@ -16,6 +17,29 @@ class DatabaseSeeder extends Seeder{
 		"Bebidas",
 		"Adicionales"
 	];
+
+	private $productos = [
+		"Hamburguesa Clásica" => 1,
+		"Hamburguesa BBQ" => 1,
+		"Hamburguesa Vegana" => 1,
+
+		"Perro Clásico" => 2,
+		"Perro con Queso" => 2,
+		"Perro Especial" => 2,
+
+		"Salchipapa Simple" => 3,
+		"Salchipapa con Bacon" => 3,
+		"Salchipapa con Queso" => 3,
+
+		"Refresco" => 4,
+		"Agua" => 4,
+		"Cerveza" => 4,
+
+		"Papas Fritas" => 5,
+		"Aros de Cebolla" => 5,
+		"Guacamole" => 5
+	];
+
 	public function run(): void{
 		$usuario = new Usuario();
 		$usuario->nombre = "Jhohan";
@@ -42,6 +66,20 @@ class DatabaseSeeder extends Seeder{
 				"orden" => $orden++,
 				"restaurantes_id" => 1
 			]);
+		}
+
+		$orden = 1;
+
+		foreach($this->productos as $producto => $categoria){
+			Producto::create([
+				"nombre" => $producto,
+				"precio" => random_int(1000, 999999),
+				"orden" => $orden++,
+				"categorias_id" => $categoria
+			]);
+			if($orden >= 4){
+				$orden = 0;
+			}
 		}
 	}
 }
